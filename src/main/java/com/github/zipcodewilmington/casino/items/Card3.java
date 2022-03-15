f656115de8242f7e150907a05cf9648111408e3f
David <davidsamcun@gmail.com>
2022-03-15 13:23:46 -0400

    test

---
+++ src/main/java/com/github/zipcodewilmington/casino/items/Card3.java
@@ -0,0 +1,26 @@
+package com.github.zipcodewilmington.casino.items;
+
+public class Card3 {
+
+    private int suit;
+    private int value;
+
+    public final static int spades = 0;
+    public final static int hearts = 1;
+    public final static int diamonds = 2;
+    public final static int clubs = 3;
+
+    public final static int ace = 1;
+    public final static int jack = 11;
+    public final static int queen = 12;
+    public final static int king = 13;
+
+    public Card3(int value, int suit){
+        if ( suit <= -1 || suit >= 4){
+            throw new IllegalArgumentException("Not a suit! Spades is 0, Hearts is 1, Diamonds is 2, and Clubs is 3");
+        }
+        if (value <= 0 || value >= 14){
+            throw new IllegalArgumentException("Not a card value! Ace is 1 and King is 13!");
+        }
+    }
+}
---
+++ src/main/java/com/github/zipcodewilmington/casino/items/DeckOfCards.java
@@ -0,0 +1,33 @@
+package com.github.zipcodewilmington.casino.items;
+
+import java.util.ArrayList;
+
+public class DeckOfCards {
+
+    private ArrayList deck;
+
+//    public DeckOfCards(){
+//
+//        this.deck = new ArrayList();
+////        for (int i = 0; i<4; i++){
+////
+////            Card value = Card.cardValue()[i];
+////
+////            for(int j = 0; j < 13; j++){
+////                Card card = new Card()
+////            }
+////        }
+//
+//        for (int i = 0; i < 13; i++){
+//            Card value = new Card.cardValue(i);
+//
+//            for (int j = 0; j < 4; j++){
+//                Card card = new Card(value, Card.Suit.values()[j]);
+//            }
+//
+//        }
+//
+//    }
+
+
+}
