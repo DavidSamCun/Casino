984ead7a6ba727dfbee62a7a3fe621b6da38673a
David <davidsamcun@gmail.com>
2022-03-15 23:26:11 -0400

    cards immutable

--- src/main/java/com/github/zipcodewilmington/casino/Person.java
+++ src/main/java/com/github/zipcodewilmington/casino/Person.java
@@ -2,6 +2,8 @@ package com.github.zipcodewilmington.casino;
 
 public class Person implements PlayerInterface{
 
+    static String Account;
+
     public CasinoAccount getArcadeAccount() {
        return null;
     }
--- src/main/java/com/github/zipcodewilmington/casino/items/Cards/Card.java
+++ src/main/java/com/github/zipcodewilmington/casino/items/Cards/Card.java
@@ -2,27 +2,27 @@ package com.github.zipcodewilmington.casino.items.Cards;
 
 public class Card {
 
-    private Suit suit;
-    private CardValue cardvalue;
+    private Suit suit;              //Private makes int inaccessable to other classes
+    private CardValue cardvalue;    //Should I add final?
 
     public Card (CardValue cardValue, Suit suit){
         this.cardvalue = cardValue;
         this.suit = suit;
     }
 
-    public Suit getSuit() {
+    public Suit getSuit() {         //Only way to get the suit
         return suit;
     }
-
-    public void setSuit(Suit suit){
+                                    //Remove setters to make immutable
+/*    public void setSuit(Suit suit){
         this.suit = suit;
-    }
+    }*/
 
     public CardValue getCardvalue(){
         return cardvalue;
     }
 
-    public void setCardvalue(CardValue cardValue){
+    /*public void setCardvalue(CardValue cardValue){
         this.cardvalue = cardValue;
-    }
+    }*/
 }
