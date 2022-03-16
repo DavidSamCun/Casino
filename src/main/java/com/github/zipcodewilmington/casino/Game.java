f8458a0897baeba06d8f89c7d425679f86785028
Ethan <ethan@zipcoders-MacBook-Pro-4.local>
2022-03-15 21:58:08 -0400

    updated abstract and interfaces

--- src/main/java/com/github/zipcodewilmington/casino/Game.java
+++ src/main/java/com/github/zipcodewilmington/casino/Game.java
@@ -1,4 +1,14 @@
 package com.github.zipcodewilmington.casino;
 
 public abstract class Game {
+    // determine if there is a winner
+    public boolean isWinner(){
+        return false;
+    }
+    //players take turns
+    public void turn(){
+
+    }
+    //
+
 }
--- src/main/java/com/github/zipcodewilmington/casino/games/roulette/RouletteEngine.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/roulette/RouletteEngine.java
@@ -1,6 +1,39 @@
 package com.github.zipcodewilmington.casino.games.roulette;
 
+import com.github.zipcodewilmington.casino.Game;
 import com.github.zipcodewilmington.casino.GameEngine;
+import com.github.zipcodewilmington.casino.Person;
+
+import java.util.List;
 
 public class RouletteEngine extends GameEngine {
+
+    public RouletteEngine(Game game, List<Person> players) {
+        super(game, players);
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public Game getGame() {
+        return null;
+    }
+
+    @Override
+    public void exitGame() {
+
+    }
+
+    @Override
+    public void replay() {
+
+    }
+
+    @Override
+    public Iterable<Person> getPlayers() {
+        return null;
+    }
 }
