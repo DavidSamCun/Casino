f7c6d8600e6c663f77b58ab93e1918d584031cc1
nusera <nusera.neha@gmail.com>
2022-03-18 18:37:23 -0400

    created slot methods

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
@@ -3,8 +3,34 @@ package com.github.zipcodewilmington.casino.games.slots;
 import com.github.zipcodewilmington.casino.GamblingInterface;
 import com.github.zipcodewilmington.casino.Game;
 
+import java.util.Random;
+
 /**
  * Created by leon on 7/21/2020.
  */
 public class SlotsGame extends Game implements GamblingInterface {
-}
+
+    private SlotReel[] reel = SlotReel.values();
+    public Random rand = new Random();
+
+    public SlotReel[] spin() {
+        SlotReel[] characters = new SlotReel[3];
+        for (int i = 0; i < characters.length; i++) {
+            characters[i] = reel[rand.nextInt(5)];
+        }
+        return characters;
+    }
+
+    public boolean isWinner(SlotReel[] result) {
+        return result[0] == result[1] && result[1] == result[2];
+    }
+
+    public String getSlotReel(SlotReel[] result) {
+        String[] printSlotReel = new String[3];
+        for (int i = 0; i < printSlotReel.length; i++) {
+            printSlotReel[i] = result[i].toString();
+        }
+        String stringSlotReel = printSlotReel[0] + "\n" + printSlotReel[1] + "\n" + printSlotReel[2] + "\n";
+        System.out.println(stringSlotReel);
+        return stringSlotReel;
+    }}
--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsPlayer.java
@@ -3,8 +3,19 @@ package com.github.zipcodewilmington.casino.games.slots;
 import com.github.zipcodewilmington.casino.Person;
 import com.github.zipcodewilmington.casino.PlayerInterface;
 
+import java.util.Scanner;
+
 /**
  * Created by leon on 7/21/2020.
  */
-public class SlotsPlayer extends Person {
+public class SlotsPlayer {
+        Scanner scanner = new Scanner(System.in);
+
+        public SlotsPlayer(String firstName, String lastName, int funds) {
+//            super(firstName, lastName, funds);
+        }
+
+        public boolean fundsCheck(int funds) {
+            return funds >= 5;
+        }
 }
