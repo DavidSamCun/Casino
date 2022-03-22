b4b3a7fb6ad8b6dba2d05fee641c4a57214ff346
nusera <nusera.neha@gmail.com>
2022-03-21 23:34:06 -0400

    davids cards

--- src/main/java/com/github/zipcodewilmington/casino/items/Cards/Hand.java
+++ src/main/java/com/github/zipcodewilmington/casino/items/Cards/Hand.java
@@ -13,4 +13,32 @@ public class Hand extends CardCollections {
         }
 
     }
+
+    public void show1Card(int i){
+        System.out.println(getCard(i).getCardRank() + " of " + getCard(i).getSuit());
+    }
+
+/*    public int BlackJhandValue() {
+        int value = 0;
+        Boolean AceExists = false;
+        for (int i = 0; i < size(); i++) {
+//            if(getCard(i).getCardRank() == CardValue.ACE){
+//                AceExists = true;
+//            }
+            if(getCard(i).getCardRank().getCardValue() > 10){
+                value+= 10;
+            } else {
+                value += getCard(i).getCardRank().getCardValue();
+            }
+        }
+        //Need to work on ACE scenario
+//        for (int i = 0; i < size(); i++){
+//            if(getCard(i).getCardRank() == CardValue.ACE && value <11){
+//                value
+//            }
+//        } //ACE scenario
+        //System.out.println(AceExists);
+        return value;
+    }*/
+
 }
